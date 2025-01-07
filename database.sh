7b22a8e83254afc7c19a486d18acb1096fb3d29b
sid2417 <siddhartha.java9398@gmail.com>
2025-01-07 17:18:47 +0530

    expense shell practice

--- database.sh
+++ database.sh
@@ -45,5 +45,35 @@ VALIDATE $? "mysql is enable  :: "
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "mysql is starting  :: "
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "mysql setting up password  :: "
+
+#--------------------------------------------------------------
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "mysql setting up password  :: "
+#--------------------------------------------------------------
+
+
+#--------------------------------------------------------------
+mysql -h db.happywithyogamoney.fun -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    VALIDATE $? "mysql setting up password  ::"
+else
+    echo "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
+#--------------------------------------------------------------
+
+
+
+#--------------------------------------------------------------
+# #Below code will be useful for idempotent nature
+# mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+# if [ $? -ne 0 ]
+# then
+#     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+#     VALIDATE $? "MySQL Root password Setup"
+# else
+#     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+# fi
+
+#--------------------------------------------------------------
